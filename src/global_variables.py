c77b4eca12914feb0dbc55ecbdacd2a717c12beb
GabiHV <84153061+GabiHV@users.noreply.github.com>
2023-06-01 20:06:20 +0200

    Actualizadas salidas redes.
    Se actualizan las redes para admitir el numero de sensor y proporcionar datos sin la variable mencionada.

--- src/global_variables.py
+++ src/global_variables.py
@@ -1,5 +1,5 @@
 # Variables globales
-INPUT_COLS_PROCESSING = ["t_ext", "h_ext", "t_C_cal","h_C_cal","t_L_cal","h_L_cal"]
+OUTPUT_COLS = ["t_ext", "h_ext", "t_C_cal","h_C_cal","t_L_cal","h_L_cal"]
 INPUT_COLS = ["t_ext", "h_ext", "t_C_cal","h_C_cal","t_L_cal","h_L_cal", 'sensor']
 
 MIN_MAX_COLS_VALUES = {"t_ext": (-50, 50), "h_ext": (0, 100), "t_C_cal": (-50, 50), 
